5a7b6d88c60c144490006f6497dc5b95344f91a7
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2022-12-14 18:15:54 +0800

    Add files via upload

--- R/diso_all.R
+++ R/diso_all.R
@@ -4,8 +4,50 @@
 #' @export
 #'
 #' @examples
-diso_all <- function(){
-    non <- cbind(cc,ae,rmse,d1,d2,d3)
+diso_all <- function(x){
+    y <- cor(x)
+    cc <- matrix(0,4,1)
+    for (i in 1:4){
+        cc[i,1] <- y[i,1]
+    }
+
+    mae <- matrix(0,4,1)
+    for (i in 1:4){
+        mae[i,1] <- caret::MAE(x[,i], x[,1])
+    }
+
+    rmse <- matrix(0,4,1)
+    for (i in 1:4){
+        rmse[i,1] <- caret::RMSE(x[,i], x[,1])
+    }
+
+    index <- cbind(cc,mae,rmse)
+
+    nor_max=function(z){
+        return(z/max(z))
+    }
+
+    nor=nor_max(index)
+
+    diso_1 <- matrix(0,4,1)  #创建空矩阵，储存结果
+    for (i in 1:4) {
+        dm1 = ((x[i,1]-x[1,1])^2)^0.5
+        diso_1[i,1] <- dm1
+    }
+
+    diso_2 <- matrix(0,4,1)  #创建空矩阵，储存结果
+    for (i in 1:3) {
+        dm2 = ((x[i,1]-x[1,1])^2+(x[i,2]-x[1,2])^2)^0.5
+        diso_2[i,1] <- dm2
+    }
+
+    diso_3 <- matrix(0,4,1)
+    for (i in 1:3) {
+        dm3 = ((x[i,1]-x[1,1])^2+(x[i,2]-x[1,2])^2+(x[i,3]-x[1,3])^2)^0.5
+        diso_3[i,1] <- dm3
+    }
+
+    non <- cbind(cc,mae,rmse,diso_1,diso_2,diso_3)
     colnames(non) <- c("CC", "AE", "RMSE","DISO_1","DISO_2","DISO_3")
     rownames(non) <- c("OBS","Model_1","Model_2","Model_3")
     return(non)
