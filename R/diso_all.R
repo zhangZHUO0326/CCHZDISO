d02e8948d62e9462775fffc7563211bda651e438
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2023-02-04 16:52:58 +0800

    Update diso_all.R

--- R/diso_all.R
+++ R/diso_all.R
@@ -6,18 +6,18 @@
 #' @examples
 diso_all <- function(x){
     y <- cor(x)
-    cc <- matrix(0,4,1)
-    for (i in 1:4){
+    cc <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)){
         cc[i,1] <- y[i,1]
     }
 
-    mae <- matrix(0,4,1)
-    for (i in 1:4){
+    mae <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)){
         mae[i,1] <- caret::MAE(x[,i], x[,1])
     }
 
-    rmse <- matrix(0,4,1)
-    for (i in 1:4){
+    rmse <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)){
         rmse[i,1] <- caret::RMSE(x[,i], x[,1])
     }
 
@@ -29,20 +29,20 @@ diso_all <- function(x){
 
    in_nor <- apply(index, 2,nor_max)
 
-    diso_1 <- matrix(0,4,1)  #创建空矩阵，储存结果
-    for (i in 1:4) {
+    diso_1 <- matrix(0,ncol(x),1)  #创建空矩阵，储存结果
+    for (i in 1:ncol(x)) {
         dm1 = ((in_nor[i,1]-in_nor[1,1])^2)^0.5
         diso_1[i,1] <- dm1
     }
 
-    diso_2 <- matrix(0,4,1)  #创建空矩阵，储存结果
-    for (i in 1:3) {
+    diso_2 <- matrix(0,ncol(x),1)  #创建空矩阵，储存结果
+    for (i in 1:ncol(x)-1) {
         dm2 = ((in_nor[i,1]-in_nor[1,1])^2+(in_nor[i,2]-in_nor[1,2])^2)^0.5
         diso_2[i,1] <- dm2
     }
 
-    diso_3 <- matrix(0,4,1)
-    for (i in 1:3) {
+    diso_3 <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)-1) {
         dm3 = ((in_nor[i,1]-in_nor[1,1])^2+(in_nor[i,2]-in_nor[1,2])^2+(in_nor[i,3]-in_nor[1,3])^2)^0.5
         diso_3[i,1] <- dm3
     }
