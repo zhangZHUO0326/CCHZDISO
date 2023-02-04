6552ebbd2e609bbb316c757bf850ddeae140bd52
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2023-02-04 16:22:32 +0800

    Update diso_ae.R

--- R/diso_ae.R
+++ R/diso_ae.R
@@ -7,8 +7,8 @@
 #'
 #' @examples
 diso_ae <- function(x){
-    mae <- matrix(0,4,1)
-    for (i in 1:4){
+    mae <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)){
         mae[i,1] <- caret::MAE(x[,i], x[,1])
     }
     return(mae)
