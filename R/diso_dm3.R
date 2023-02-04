0bb170ce65694b755101ba3b793e509879b65787
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2023-02-04 17:07:27 +0800

    Update diso_dm3.R

--- R/diso_dm3.R
+++ R/diso_dm3.R
@@ -7,8 +7,8 @@
 #'
 #' @examples
 diso_dm3 <- function(x){
-    diso_3 <- matrix(0,4,1)
-    for (i in 1:3) {
+    diso_3 <- matrix(0,ncol(x),1)
+    for (i in 1:ncol(x)) {
         dm3 = ((x[i,1]-x[1,1])^2+(x[i,2]-x[1,2])^2+(x[i,3]-x[1,3])^2)^0.5
         diso_3[i,1] <- dm3
         }
