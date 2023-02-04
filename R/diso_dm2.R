2962abfb75881c2c34229ed24d6a88bc514392dd
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2023-02-04 17:07:06 +0800

    Update diso_dm2.R

--- R/diso_dm2.R
+++ R/diso_dm2.R
@@ -7,8 +7,8 @@
 #'
 #' @examples
 diso_dm2 <- function(x){
-    diso_2 <- matrix(0,4,1)  #创建空矩阵，储存结果
-for (i in 1:3) {
+    diso_2 <- matrix(0,ncol(x),1)  #创建空矩阵，储存结果
+for (i in 1:ncol(x)) {
     dm2 = ((x[i,1]-x[1,1])^2+(x[i,2]-x[1,2])^2)^0.5
     diso_2[i,1] <- dm2
 }
