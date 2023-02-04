0e9341b506ae69a9be3949afe879a3a2eb50e7df
zhangZHUO0326 <119782097+zhangZHUO0326@users.noreply.github.com>
2023-02-04 16:56:35 +0800

    Update diso_all.R

--- R/diso_all.R
+++ R/diso_all.R
@@ -36,19 +36,19 @@ diso_all <- function(x){
     }
 
     diso_2 <- matrix(0,ncol(x),1)  #创建空矩阵，储存结果
-    for (i in 1:ncol(x)-1) {
+    for (i in 1:ncol(x)) {
         dm2 = ((in_nor[i,1]-in_nor[1,1])^2+(in_nor[i,2]-in_nor[1,2])^2)^0.5
         diso_2[i,1] <- dm2
     }
 
     diso_3 <- matrix(0,ncol(x),1)
-    for (i in 1:ncol(x)-1) {
+    for (i in 1:ncol(x)) {
         dm3 = ((in_nor[i,1]-in_nor[1,1])^2+(in_nor[i,2]-in_nor[1,2])^2+(in_nor[i,3]-in_nor[1,3])^2)^0.5
         diso_3[i,1] <- dm3
     }
 
     non <- cbind(cc,mae,rmse,diso_1,diso_2,diso_3)
     colnames(non) <- c("CC", "AE", "RMSE","DISO_1","DISO_2","DISO_3")
-    rownames(non) <- c("OBS","Model_1","Model_2","Model_3")
+
     return(non)
 }
